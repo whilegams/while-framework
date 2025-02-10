619013a6985fb5d2e4b2a9bd4246c7d1db6f92f0
Vasily Mironov <whilesocold@gmail.com>
2025-02-10 16:32:55 +0300

    fixes

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,3 +1,4 @@
 export { type AppServiceOptions, AppService, appService } from './AppService';
 export { AssetService, assetService } from './AssetService';
 export { Page, Modal, PageService, pageService } from './PageService';
+export { LocaleService, localeService } from './LocaleService';
